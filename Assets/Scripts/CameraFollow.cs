9a474a1da2ed1d3862ae0f7966da108e8c69c2b9
Apheayenne <zelda.ganondorf@gmail.com>
2021-03-16 18:21:29 -0400

    Camera Follow
    The Camera will now be able to follow the Character

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform character;
+    public Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(character.position.x + offset.x, character.position.y + offset.y, offset.z);
+    }
+}
