9afdb9aa304815beb56db58cc37bf9f07033b1c4
King Maxwell <mmdawg1@aol.com>
2021-04-07 12:43:30 -0400

    Setting up class structure
    Just getting scripts made don't have any functionality quite yet except pre implemented things
    
    Structure:
    
    Entity>LivingEntity>(Player,(NPC>Enemy)), (InteractionObject>'SpecialFloor','MoveableObject')
    
    Item>Literally any item

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -2,9 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemDatabase : ScriptableObject
+public class Entity : MonoBehaviour
 {
-
-
 
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : LivingEntity
+
+{
+
+    public float playerSpeed = 1f;
+
+    PlayerAnimation playAni;
+
+    // Start is called before the first frame update
+    public void Awake()
+    {
+        playAni = GetComponentInChildren<PlayerAnimation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 movement = Vector2.ClampMagnitude(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")), 1) * getSpeed();
+        Vector2 newPosition = GetComponent<Rigidbody2D>().position + movement * Time.fixedDeltaTime;
+
+        playAni.setDirection(movement);
+        GetComponent<Rigidbody2D>().MovePosition(newPosition);
+    }
+}
