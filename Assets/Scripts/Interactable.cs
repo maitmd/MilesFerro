310bcaff08b738c74bfb4b9a5b7d7dc30e1e632f
Apheayenne <zelda.ganondorf@gmail.com>
2021-04-27 18:29:01 -0400

    Reworked Maps

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -1,11 +1,7 @@
 using UnityEngine;
 
-//[RequireComponent(typeof(BoxCollider2D))]
 public abstract class Interactable : MonoBehaviour
 {
-	/*private void Reset() {
-		//GetComponent<BoxCollider2D>().isTrigger = true;
-	}*/
 	
 	public abstract void Interact();
 
