b25673cbb6a63dfeccad6c2cd22a1c7798bb949d
King Maxwell <mmdawg1@aol.com>
2021-04-27 23:34:13 -0400

    push

--- Assets/Scripts/BattleUI/HealthBarUI.cs
+++ Assets/Scripts/BattleUI/HealthBarUI.cs
@@ -10,14 +10,14 @@ using UnityEngine.UI;
 		public Gradient gradient;
 		public Image fill;
 
-		public void SetMaxHealth(int maxHealth) {
+		public void SetMaxHealth(float maxHealth) {
 
 			slider.maxValue = maxHealth;
 			slider.value = maxHealth;
 			fill.color = gradient.Evaluate(1f);
 		}
 
-		public void SetHealth(int health) {
+		public void SetHealth(float health) {
 
 			slider.value = health;
 			fill.color = gradient.Evaluate(slider.normalizedValue);
