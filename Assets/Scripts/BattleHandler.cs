a3873eaec1f349a6f0e362b956c77f4716ef1ec3
Apheayenne <zelda.ganondorf@gmail.com>
2021-04-27 23:29:18 -0400

    more shit

--- Assets/Scripts/BattleHandler.cs
+++ Assets/Scripts/BattleHandler.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class BattleHandler : MonoBehaviour
-{
+public class BattleHandler : MonoBehaviour {
     [SerializeField] private Enemy[] enemyBattlers;
     [SerializeField] private Player[] allyBattlers;
 
@@ -21,8 +20,7 @@ public class BattleHandler : MonoBehaviour
     private int enemyIndex;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         fillEnemies(enemyBattlers[0]);
         fillAllies(allyBattlers[0]);
         allyIndex = 0;
@@ -33,28 +31,21 @@ public class BattleHandler : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (playerTurn)
-        {
+    void Update() {
+        if(playerTurn) {
             playerPanel.SetActive(true);
             allyIndex++;
-        }
-        else
-        {
-            if(allyIndex >= allyBattlers.Length)
-            {
+        } else {
+            if(allyIndex >= allyBattlers.Length) {
                 allyIndex = 0;
             }
 
-            if (enemyIndex >= enemyBattlers.Length)
-            {
+            if(enemyIndex >= enemyBattlers.Length) {
                 enemyIndex = 0;
             }
 
             playerPanel.SetActive(false);
-            if (!(allyBattlers[allyIndex] is Player))
-            {
+            if(!(allyBattlers[allyIndex] is Player)) {
                 float randAlly = Random.Range(0f, ((float)(enemyBattlers.Length)));
                 allyBattlers[allyIndex].battleActions(enemyBattlers[(int)randAlly]);
             }
@@ -71,58 +62,28 @@ public class BattleHandler : MonoBehaviour
         updateHealth();
     }
 
-    public void fillEnemies(Enemy en)
-    {
+    public void fillEnemies(Enemy en) {
         //pull from stored arrayList of enemies in Enemy [Not implemented in Enemy]
     }
 
-    public void fillAllies(Player pl)
-    {
-        
+    public void fillAllies(Player pl) {
+
         //possibly in the future you will have allies in the game I would suggest making an Ally class and derive Player from it
         //additionally just like Enemies will store their mob in the main enemy have the player store allies in an arraylist as well
     }
 
-    public void switchTurn()
-    {
+    public void switchTurn() {
         playerTurn = !playerTurn;
     }
 
-    public void updateHealth()
-    {
-        for(int i = 0; i < allyIndex; i++)
-        {
+    public void updateHealth() {
+        for(int i = 0; i < allyIndex; i++) {
             playerHealthBar.SetHealth(allyBattlers[i].CurrentHealth);
         }
 
-        for (int i = 0; i < enemyIndex; i++)
-        {
+        for(int i = 0; i < enemyIndex; i++) {
             enemyHealthBar.SetHealth(enemyBattlers[i].CurrentHealth);
         }
+
     }
-
-    public void fillEnemies(Enemy en) {
-	//pull from stored arrayList of enemies in Enemy [Not implemented in Enemy]
-    }
-
-public void fillAllies(Player pl) {
-
-	//possibly in the future you will have allies in the game I would suggest making an Ally class and derive Player from it
-	//additionally just like Enemies will store their mob in the main enemy have the player store allies in an arraylist as well
-}
-
-public void switchTurn() {
-	playerTurn = !playerTurn;
-}
-
-public void updateHealth() {
-	for(int i = 0; i < allyIndex; i++) {
-		playerHealthBar.SetHealth(allyBattlers[i].CurrentHealth);
-	}
-
-	for(int i = 0; i < enemyIndex; i++) {
-		enemyHealthBar.SetHealth(enemyBattlers[i].CurrentHealth);
-	}
-
-}
 }
